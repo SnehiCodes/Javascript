11c37e5da914aeca6ce857423224a1cca5950ccd
SnehiCodes <111358189+SnehiCodes@users.noreply.github.com>
2022-09-06 08:34:22 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,8 @@
+document.write("<h1 style ='color:Green;'>Text");
+document.write("<hr/>");
+
+var phrase = "To be or not to be";
+document.write(phrase);
+
+var phrase = "Snehi is a great person";
+document.write(phrase);
