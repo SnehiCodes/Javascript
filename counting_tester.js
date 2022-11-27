3d2c23ce30dbcaf667e5eaf47d9fa2b1060990a8
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-11-28 00:09:35 +0530

    Add files via upload

---
+++ counting_tester.js
@@ -0,0 +1,9 @@
+// counting tester
+
+var str = "meekoman";
+var pos = str.indexOf("e");
+console.log(pos);
+var mypos = str.indexOf("e",pos+1);
+console.log(mypos);
+var mypos1 = str.indexOf("e",mypos+1);
+console.log(mypos1);
