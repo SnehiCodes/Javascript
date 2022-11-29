9c1b932f8bf45eecf694062816e8a31929772190
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-11-29 17:08:06 +0530

    Add files via upload

---
+++ iteration.js
@@ -0,0 +1,15 @@
+// Iteration of an array
+
+array= [0,1,2,3,4,5,6,7,8];
+
+//   for(variables; condition ; modification)
+for(var i=0;i<array.length;i++)
+{
+    console.log(array[i]);
+}
+
+// while loop
+var counter =0;
+while(counter<array.length){
+    console.log(array[i]);
+}
